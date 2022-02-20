86196a37a93fb84780bfb529aed405d0dc702265
x7429 <94696817+x7429@users.noreply.github.com>
2022-02-20 08:35:12 +0100

    Update telescope.lua

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -1,6 +1,23 @@
+local ret, telescope = pcall(require, "telescope")
+
 vim.cmd [[
 	nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 	nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 	nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 	nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
 ]]
+
+telescope.setup{
+	defaults = {
+		hidden = false,
+		mappings = {
+		}
+	},
+	pickers = {
+		find_files = {
+			hidden = true
+		}
+	},
+	extensions = {
+	}
+}
