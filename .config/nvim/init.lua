52012d8c6137357fa5ab2aeafadcc3e29bea1c5c
x7429 <94696817+x7429@users.noreply.github.com>
2022-02-25 07:55:25 +0100

    Delete .config/nvim directory

--- .config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require('user.plugins')
-require('user.options')
-require('user.lualine')
-require('user.telescope')
-require('user.functions')
--- .config/nvim/lua/user/functions.lua
+++
@@ -1,4 +0,0 @@
-function getIp()
-	local ip = io.popen("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep -Eo '([0-9]*\\.){3}[0-9]*' | grep -v '127.0.0.1'"):read("*all")
-	vim.cmd("normal i" .. ip)
-end
